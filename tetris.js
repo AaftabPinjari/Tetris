f394a4862abd0d4989b3104a0a0a2c75a7f3a71c
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-05-02 18:47:07 +0530

    done with tetris

--- tetris.js
+++ tetris.js
@@ -2,11 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
-    const width = 10;
-    const squareDisplay = document.querySelector('#score')
+    const scoreDisplay = document.querySelector('#score')
     const startButton = document.querySelector('#start-button')
+    const width = 10;
     let nextRandom = 0
+    let timeId
+    let score = 0
 
+
+    //assigning functions to keycodes
+    function controls(e) {
+        if (e.keyCode === 37) {
+            moveLeft()
+        }
+        if (e.keyCode === 39) {
+            moveRight()
+        }
+        if (e.keyCode === 40) {
+            moveDown()
+        }
+        if (e.keyCode === 38) {
+            rotate()
+        }
+    }
+    document.addEventListener('keyup', controls)
     //each tetrimino 
     const lTetromino = [
         [1, width + 1, width * 2 + 1, 2],
@@ -47,27 +66,10 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentPosition = 4
     let currentRotation = 0
     //randomly selecting the tetromino
-
     let random = Math.floor(Math.random() * theTetrominoes.length)
-
     let current = theTetrominoes[random][currentRotation]
 
-    //assigning functions to keycodes
-    function controls(e) {
-        if (e.keyCode === 37) {
-            moveLeft()
-        }
-        if (e.keyCode === 39) {
-            moveRight()
-        }
-        if (e.keyCode === 40) {
-            moveDown()
-        }
-        if (e.keyCode === 38) {
-            rotate()
-        }
-    }
-    document.addEventListener('keyup', controls)
+
 
 
     //drawing first rotation for first tetromino
@@ -87,7 +89,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     //tetromino down movement
-    timeId = setInterval(moveDown, 1000)
 
 
 
@@ -110,6 +111,8 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPosition = 4
             draw()
             displayShape()
+            addScore()
+            gameOver()
         }
     }
 
@@ -179,6 +182,43 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    //start/pause btn
+    startButton.addEventListener('click', () => {
+        if (timeId) {
+            clearInterval(timeId)
+            timeId = null
+        } else {
+            draw()
+            timeId = setInterval(moveDown, 1000)
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+            displayShape()
+        }
+    })
+    // score
 
+    function addScore() {
+        for (let i = 0; i < 199; i += width) {
+            const row = [i, i + 1, i = 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
+
+            if (row.every(index => squares[index].classList.contains('taken'))) {
+                score += 10
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                    squares[index].classList.remove('tetromino')
+                })
+                const squaresRemoved = squares.splice(i, width)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+    }
+    //game over
+    function gameOver() {
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            scoreDisplay.innerHTML = 'end'
+            clearInterval(timeId)
+        }
+    }
 
 })
